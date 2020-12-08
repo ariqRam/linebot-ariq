374cdf28cd144839b497b50fb070c038ecfe75dc
ariqathallah38 <ariqathallah38@gmail.com>
2020-12-08 20:09:58 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const express = require('express')
+const line = require('@line/bot-sdk')
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+}
+const client = new line.Client(config)
+
+const app = express();
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    if (!Array.isArray(req.body.events)) {
+      return res.status(500).end();
+    }
+    Promise
+      .all(req.body.events.map(event => {
+          if (event.replyToken === '00000000000000000000000000000000' ||
+            event.replyToken === 'ffffffffffffffffffffffffffffffff') {
+            return;
+          }
+          return handleEvent(event);
+      }))
+      .then(()=>res.end())
+      .catch((err)=>{
+        console.error(err);
+        res.status(500).end();
+      })
+});
+
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+};
+
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          if(event.message.text[0]=='!'){
+            return replyText(event.replyToken, 'apaan sih');
+          }
+          else{
+            return handleText(message, event.replyToken)
+          }
+        case 'image':
+          return handleImage(message, event.replyToken);
+        case 'video':
+          return handleVideo(message, event.replyToken);
+        case 'audio':
+          return handleAudio(message, event.replyToken);
+        case 'location':
+          return handleLocation(message, event.replyToken);
+        case 'sticker':
+          return handleSticker(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return replyText(event.replyToken, 'Got followed event');
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'join':
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+    case 'leave':
+      return console.log(`Left: ${JSON.stringify(event)}`);
+
+    case 'postback':
+      let data = event.postback.data;
+      return replyText(event.replyToken, `Got postback: ${data}`);
+
+    case 'beacon':
+      const dm = `${Buffer.from(event.beacon.dm || '', 'hex').toString('utf8')}`;
+      return replyText(event.replyToken, `${event.beacon.type} beacon hwid : ${event.beacon.hwid} with device message = ${dm}`);
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+function handleText(message, replyToken) {
+  return replyText(replyToken, message.text);
+}
+
+function handleImage(message, replyToken) {
+  return replyText(replyToken, 'Got Image');
+}
+
+function handleVideo(message, replyToken) {
+  return replyText(replyToken, 'Got Video');
+}
+
+function handleAudio(message, replyToken) {
+  return replyText(replyToken, 'Got Audio');
+}
+
+function handleLocation(message, replyToken) {
+  return replyText(replyToken, 'Got Location');
+}
+
+function handleSticker(message, replyToken) {
+  return replyText(replyToken, 'Got Sticker');
+}
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
